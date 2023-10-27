b2d99fb5f27ecd249e93238264d4c1c225d1e43b
amit.khetan.70 <amit.khetan.70@50gramx.io>
2023-10-27 07:41:33 +0530

    fix import

--- src/eapp_python_consumer/gramx/fifty/zero/ethos/identity/multiverse/core/entity/account/capabilities/access/service.py
+++ src/eapp_python_consumer/gramx/fifty/zero/ethos/identity/multiverse/core/entity/account/capabilities/access/service.py
@@ -1,7 +1,7 @@
 from ethos.elint.services.product.identity.account.access_account_pb2 import ValidateAccountRequest
 from ethos.elint.services.product.identity.account.access_account_pb2_grpc import AccessAccountServiceStub
 
-from fifty.zero.ethos.identity.multiverse.core.entity.account.capabilities.access import channel
+from gramx.fifty.zero.ethos.identity.multiverse.core.entity.account.capabilities.access import channel
 
 
 class AccessAccountService:
